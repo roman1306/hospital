824ef8e01be046f8fc6b2c58fb93f0df0398deb2
DESKTOP-KI3T2DP\fires <rpetrenko51@gmail.com>
2020-07-31 13:29:53 +0400

    add description in README.md, and add prePostEnabled in Application

--- src/main/java/com/github/roman1306/registry/RegistryApplication.java
+++ src/main/java/com/github/roman1306/registry/RegistryApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
-@EnableGlobalMethodSecurity(securedEnabled = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class RegistryApplication {
 
     public static void main(String[] args) {
